0d78e1aed28cb2a0d3bf0557438d7711cd529dc4
kartik_32 <chaudharykartik2005@gmail.com>
2026-06-09 13:36:38 +0530

    basic final file for both stages

--- notification_app_fe/app/page.tsx
+++ notification_app_fe/app/page.tsx
@@ -1,54 +1,39 @@
-"use client";
+import axios from "axios";
 
-import { useEffect, useState } from "react";
+export default async function Home() {
 
-import { getNotifications }
-from "../services/api";
-
-import NotificationCard
-from "../components/NotificationCard";
-
-export default function Home() {
-
-  const [notifications,
-    setNotifications] =
-    useState<any[]>([]);
-
-  useEffect(() => {
-
-    async function load() {
-
-      const data =
-        await getNotifications();
-
-      setNotifications(data);
-
+  const response = await axios.get(
+    "http://4.224.186.213/evaluation-service/notifications",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`
+      }
     }
+  );
 
-    load();
-
-  }, []);
+  const notifications =
+    response.data.notifications;
 
   return (
-
     <div>
+      <h1>Notifications</h1>
 
-      <h1>
-        Notifications
-      </h1>
+      {notifications.map((item: any) => (
+        <div
+          key={item.ID}
+          style={{
+            border: "1px solid gray",
+            margin: "10px",
+            padding: "10px"
+          }}
+        >
+          <h3>{item.Message}</h3>
 
-      {
-        notifications.map(
-          (item) => (
-            <NotificationCard
-              key={item.ID}
-              notification={item}
-            />
-          )
-        )
-      }
+          <p>{item.Type}</p>
 
+          <p>{item.Timestamp}</p>
+        </div>
+      ))}
     </div>
-
   );
 }
